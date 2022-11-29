6aec400e1b165085fc0a28f4d85e26de16de62ec
Ssa! <104830931+saJaeHyukc@users.noreply.github.com>
2022-11-29 17:13:31 +0900

    Refact : 공백란 삭제

--- Music_Recommend/playlists/views.py
+++ Music_Recommend/playlists/views.py
@@ -25,6 +25,7 @@ class PlaylistView(APIView):
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class PlaylistDetailView(APIView):
     permission_classes = [IsAuthenticated]
     
